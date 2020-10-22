118129487d5d0ec7ceebcf0b1808dbeb2c7b77a8
Cheeun <cheeun0914@gmail.com>
2020-10-22 22:48:17 +0900

    added network architecture files

---
+++ src/test.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=7 python main.py \
+--model EDSR --scale 4 \
+--lr 1e-4 --epochs 300 --decay 150  --loss 1*L1 \
+--data_test Set5+Set14+B100+Urban100 --data_range 1-800/801-810 \
+--batch_size 8 --patch_size 192 \
+--n_threads 1 --n_GPUs 1 \
+--save fdsr_test \
+--searched_model fdsr_full_x4_50G \
+--n_feats 256 --n_resblocks 32 --res_scale 0.1 \
+--test_only \
+--pre_train PRETRAINED_DIRECTORY \
+#--dir_data ~/workspace/datasets/ \
